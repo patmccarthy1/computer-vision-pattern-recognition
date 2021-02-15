b025dbc8808e0e66c0013604397a72407c2803f3
mariaarranz <56388787+mariaarranz@users.noreply.github.com>
2021-02-15 20:46:10 +0100

    comment code

--- task_2/matching_desciptors.py
+++ task_2/matching_desciptors.py
@@ -1,3 +1,4 @@
+# reference: https://blog.francium.tech/feature-detection-and-matching-with-opencv-5fd2394a590
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
@@ -10,14 +11,14 @@ img2 = cv2.imread('HG_no_grid_10.jpg', 0)
 img1= cv2.imread('HG_06.jpg', 0)
 img2 = cv2.imread('HG_03.jpg', 0)
 
-# Uses ORB descriptors to compute the matches
+# Uses ORB descriptors to compute the feature matches in each image
 orb = cv2.ORB_create(nfeatures=500)
 kp1, des1 = orb.detectAndCompute(img1, None)
 kp2, des2 = orb.detectAndCompute(img2, None)
 
 # matcher takes normType, cv2.NORM_HAMMING for ORB, FAST and BRIEF
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-matches = bf.match(des1, des2)
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True) # brute-force matcher function
+matches = bf.match(des1, des2) # matches the points between images
 matches = sorted(matches, key=lambda x: x.distance)
 
 # draw first 50 matches
@@ -26,9 +27,8 @@ match_img = cv2.drawMatches(img1, kp1, img2, kp2, matches[:50], None)
 # Save Images (uncomment corresponding to image used)
 '''
 cv2.imwrite('matches_no_grid.jpg', match_img)
-'''
 cv2.imwrite('matches_grid.jpg', match_img)
-
+'''
 # Show the images
 cv2.imshow('Matches', match_img)
 cv2.waitKey()
